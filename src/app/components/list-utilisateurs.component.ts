a15f6392586e683ae3af3c34a53ae9aacdae6dc1
Lucas <lucasvaugrente35@gmail.com>
2025-12-01 17:46:34 +0100

    Add List Pagination Users with navBar Navigation on the top (IT WORKS)

--- src/app/components/list-utilisateurs.component.ts
+++ src/app/components/list-utilisateurs.component.ts
@@ -1,35 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Utilisateur } from '../models/utilisateurDTO';
 import { UtilisateurService } from '../services/utilisateur.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-utilisateur-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, RouterLink],
   template: `
-    <div class="card my-5">
-      <div class="card-body">
-        <table class="table table-bordered table-striped">
-          <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nom</th>
-            <th scope="col">Prenom</th>
-            <th scope="col">Mail</th>
-            <th scope="col">Nom d'utilisateur</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr *ngFor="let utilisateur of utilisateurs">
-            <td>{{ utilisateur.id }}</td>
-            <td>{{ utilisateur.nom }}</td>
-            <td>{{ utilisateur.prenom }}</td>
-            <td>{{ utilisateur.mail }}</td>
-            <td>{{ utilisateur.username }}</td>
-          </tr>
-          </tbody>
+    <div>
+      <div>
+
+        <div class="block-btn">
+          <a routerLink="/addUser" class="btn">Ajouter un utilisateur</a>
+        </div>
+
+        <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">
+
+          <ng-container matColumnDef="id">
+            <th mat-header-cell *matHeaderCellDef> # </th>
+            <td mat-cell *matCellDef="let utilisateur"> {{ utilisateur.id }} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="nom">
+            <th mat-header-cell *matHeaderCellDef> Nom </th>
+            <td mat-cell *matCellDef="let utilisateur"> {{ utilisateur.nom }} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="prenom">
+            <th mat-header-cell *matHeaderCellDef> Prenom </th>
+            <td mat-cell *matCellDef="let utilisateur"> {{ utilisateur.prenom }} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="mail">
+            <th mat-header-cell *matHeaderCellDef> Mail </th>
+            <td mat-cell *matCellDef="let utilisateur"> {{ utilisateur.mail }} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="username">
+            <th mat-header-cell *matHeaderCellDef> Nom d'utilisateur </th>
+            <td mat-cell *matCellDef="let utilisateur"> {{ utilisateur.username }} </td>
+          </ng-container>
+
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
         </table>
+
+        <mat-paginator [pageSizeOptions]="[5, 10, 20]"
+                       showFirstLastButtons
+                       aria-label="Sélectionner la page des utilisateurs">
+        </mat-paginator>
+
       </div>
     </div>
   `,
@@ -67,17 +91,50 @@ import { UtilisateurService } from '../services/utilisateur.service';
       background: #F8F8F8;
     }
 
+    .block-btn {
+      margin: 20px;
+    }
+
+    .btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #405cf5;
+      border-radius: 6px;
+      box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .1) 0 2px 5px 0, rgba(0, 0, 0, .07) 0 1px 1px 0;
+      padding: 15px;
+      color: white;
+      text-decoration: none;
+      width: 150px;
+      font-weight: bold;
+      transition: 0.2s ease-in-out;
+    }
+
+    .btn:hover {
+      background-color: #354fdf;
+    }
+
   `]
 })
-export class UtilisateurListComponent implements OnInit {
+export class UtilisateurListComponent implements OnInit, AfterViewInit { // 3. Implémenter AfterViewInit
 
-  utilisateurs: Utilisateur[] | undefined;
+  displayedColumns: string[] = ['id', 'nom', 'prenom', 'mail', 'username'];
+
+  dataSource = new MatTableDataSource<Utilisateur>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private utilisateurService: UtilisateurService) { }
 
   ngOnInit() {
     this.utilisateurService.getUtilisateurs().subscribe(data => {
-      this.utilisateurs = data;
+      // 6. Affecter les données à la source de la table
+      this.dataSource.data = data;
     });
+  }
+
+  // 7. Affecter le paginator à la source de données après l'initialisation de la vue
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
