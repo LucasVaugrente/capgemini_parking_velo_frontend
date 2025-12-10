3c91239149b376bec310b5e049e15fc12d121f3e
Lucas <lucasvaugrente35@gmail.com>
2025-12-10 15:24:43 +0100

    fix bug ng test

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,10 +10,18 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
-    
+      imports: [NavbarComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: { get: () => 'mockParam' } },
+            queryParams: of({}),
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
