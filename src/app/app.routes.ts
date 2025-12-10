c66cd04d87b7cc7af252a23e82817995b9901282
Lucas <lucasvaugrente35@gmail.com>
2025-12-10 15:28:15 +0100

    add a home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UtilisateurListComponent } from './pages/list-utilisateurs.component';
+import { UtilisateurListComponent } from './pages/list-utilisateurs';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HomeComponent} from "./pages/home";
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Page d'accueil
   { path: 'users', component: UtilisateurListComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' } // Redirection par défaut
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes), ReactiveFormsModule],
   exports: [RouterModule],
