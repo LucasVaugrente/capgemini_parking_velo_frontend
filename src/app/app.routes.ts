ccc905ce8dd8593a8fe51b3235c823331dde3b12
Lucas <lucasvaugrente35@gmail.com>
2025-12-12 15:36:16 +0100

    add velos on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { UtilisateurListComponent } from './pages/list-utilisateurs';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HomeComponent} from "./pages/home";
+import {VeloListComponent} from "./pages/list-velos";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Page d'accueil
   { path: 'users', component: UtilisateurListComponent },
+  { path: 'velos', component: VeloListComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' } // Redirection par défaut
 ];
 @NgModule({
