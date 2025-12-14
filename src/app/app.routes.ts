7add2dd2155ef82dc55532de941ae219172f5084
Salma <salma.mansouri@etu.univ-tours.fr>
2025-12-14 21:19:13 +0100

    login page : component + service + l'ajout Déconnexion + auth.guard.ts + auth.guard.spec.ts + petite modif dans app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UtilisateurListComponent } from './pages/list-utilisateurs';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HomeComponent} from "./pages/home";
+import {LoginComponent} from "./components/LoginComponent";
+import {AuthGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }, // Page d'accueil
-  { path: 'users', component: UtilisateurListComponent },
+  { path: 'users', component: UtilisateurListComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/', pathMatch: 'full' } // Redirection par défaut
 ];
 @NgModule({
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/Auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
